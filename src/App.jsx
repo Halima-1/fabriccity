37bba8f7443da42e83a1f2bcf28f83b974008027
dahunsi halima <dahunsiolajumoke18@gmail.com>
2024-12-15 20:15:28 +0100

    modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 // import { useState } from "react";
-import "./styles/product.css"
+import "./styles/product.css";
 import Product from "./pages/productPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductDetails from "./component/productDetails";
 import Login from "./component/form/login";
 import Register from "./component/form/register";
-import Header from "./component/layout/header"
+import Header from "./component/layout/header";
 import Footer from "./component/layout/footer";
 import Home from "./component/home/home";
 import Cart from "./pages/cart/cart";
@@ -14,21 +14,21 @@ import Wishlist from "./pages/wishlist/wishlist";
 import { Outlet } from "react-router-dom";
 function App() {
   // const [count, setCount] = useState(0);
- const Layout = () => {
-   return (
-     <>
-       <Header />
-       <Outlet />
-       <Footer />
-     </>
-   );
- };
+  const Layout = () => {
+    return (
+      <>
+        <Header />
+        <Outlet />
+        <Footer />
+      </>
+    );
+  };
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/product" element={<Product />} />
+          <Route path="/" element={<Product />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/wishlist" element={<Wishlist />} />
 
